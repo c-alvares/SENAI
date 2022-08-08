905b50d3fbec27830f6fa75b71b8d54b68eee980
c-alvares <cyrobrandaoalvares@gmail.com>
2022-08-08 11:59:55 -0300

    Correção do Exercício Aula 5, adicionando proposta de melhoria do código. Aula 6 com exercício

---
+++ 2DES/PWFE/AULA05EXC/script.js
@@ -0,0 +1,9 @@
+var inputNome = document.querySelector("#nome");
+var inpCargo = document.querySelector("#cargo");
+var inpSalario = document.querySelector("#salario");
+
+var tbody = document.querySelector("#table-body");
+
+function cadastrar() {
+    
+}
