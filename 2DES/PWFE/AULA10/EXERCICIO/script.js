343905c6d31eab3f4a7efa2de5f2b08ba6e6416b
c-alvares <cyrobrandaoalvares@gmail.com>
2022-08-17 10:59:42 -0300

    Aula 10 com início do exercício

---
+++ 2DES/PWFE/AULA10/EXERCICIO/script.js
@@ -0,0 +1,33 @@
+var name = document.getElementById("nome");
+var mail = document.getElementById("email");
+var password = document.getElementById("senha");
+var repPassword = document.getElementById("repeteSenha");
+var mobile = document.getElementById("telefone");
+var zip = document.getElementById("cep");
+var street = document.getElementById("logradouro");
+var complement = document.getElementById("complemento");
+var neighborhood = document.getElementById("bairro");
+var city = document.getElementById("cidade");
+var state = document.getElementById("estado");
+
+// function cadastrar() {    
+//     let userData = {
+//         "nome": ,
+//         "email": ,
+//         "senha": ,
+//         "repeteSenha": ,
+//         "telefone": ,
+//         "cep": {
+//             "logradouro": ,
+//             "complemento": ,
+//             "bairro": ,
+//             "cidade": ,
+//             "estado": 
+//         }
+//     }
+// }
+
+function buscaCep () {
+    
+}
+// https://viacep.com.br/
