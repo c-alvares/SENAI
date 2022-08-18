bef387ea09360758a806ff2bae00f6e332c6d14d
Cyro Alvares <cyrobrandaoalvares@gmail.com>
2022-08-17 21:00:47 -0300

    Executada função para busca de cep através de consumo de API

--- 2DES/PWFE/AULA10/EXERCICIO/script.js
+++ 2DES/PWFE/AULA10/EXERCICIO/script.js
@@ -3,21 +3,21 @@ var mail = document.getElementById("email");
 var password = document.getElementById("senha");
 var repPassword = document.getElementById("repeteSenha");
 var mobile = document.getElementById("telefone");
-var zip = document.getElementById("cep");
-var street = document.getElementById("logradouro");
-var complement = document.getElementById("complemento");
-var neighborhood = document.getElementById("bairro");
-var city = document.getElementById("cidade");
-var state = document.getElementById("estado");
-
+var zip = document.querySelector("#caixaPostal");
 
 function buscaCep () {
+    
     fetch("https://viacep.com.br/ws/" + zip.value + "/json/")
     .then(response => { return response.json() })
     .then(data => {
         if(zip.value == data.cep) {
-        street.innerHTML = data.logradouro;    
+        document.querySelector('#logradouro').value = data.logradouro;
+        document.querySelector('#complemento').value = data.complemento;
+        document.querySelector('#bairro').value = data.bairro;   
+        document.querySelector('#cidade').value = data.localidade;   
+        document.querySelector('#estado').value = data.uf;       
         }
+       
     })
 
 }
