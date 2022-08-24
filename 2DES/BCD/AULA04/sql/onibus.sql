08b93f92c3baf2d3c2b1dc412ca07862cc0fbd31
c-alvares <cyrobrandaoalvares@gmail.com>
2022-08-24 11:27:55 -0300

    Continuação da aula 04 de banco de dados, realizados na aula 05

---
+++ 2DES/BCD/AULA04/sql/onibus.sql
@@ -0,0 +1,64 @@
+drop database if exists onibus;
+create database onibus charset=UTF8 collate utf8_general_ci;
+use onibus;
+
+create table motoristas(
+    cpf varchar(11) not null primary key,
+    nome varchar(50) not null
+);
+create table telefones(
+    cpf varchar(11) not null,
+    numero varchar(15) not null,
+    foreign key (cpf) references motoristas(cpf)
+);
+create table linhas(
+    id_linha varchar(10) not null primary key,
+    descricao varchar(50) not null
+);
+create table horarios(
+    id_linha varchar(10) not null,
+    horario time not null,
+    foreign key (id_linha) references linhas(id_linha)
+);
+create table mot_linhas(
+    cpf varchar(11) not null,
+    id_linha varchar(10) not null,
+    data date not null,
+    foreign key (cpf) references motoristas(cpf),
+    foreign key (id_linha) references linhas(id_linha)
+);
+
+describe motoristas;
+describe telefones;
+describe linhas;
+describe horarios;
+describe mot_linhas;
+
+show tables;
+
+LOAD DATA INFILE 'C:/Users/Desenvolvimento/Desktop/SENAI/2DES/BCD/AULA04/sql/csv/onibus/motoristas.csv'
+INTO TABLE motoristas
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+select * from telefones;
+
+LOAD DATA INFILE 'C:/Users/Desenvolvimento/Desktop/SENAI/2DES/BCD/AULA04/sql/csv/onibus/telefones.csv'
+INTO TABLE telefones
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+select * from telefones;
+
+LOAD DATA INFILE 'C:/Users/Desenvolvimento/Desktop/SENAI/2DES/BCD/AULA04/sql/csv/onibus/telefones.csv'
+INTO TABLE telefones
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+select * from telefones;
