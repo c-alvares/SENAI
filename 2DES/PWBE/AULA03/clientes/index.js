fd2e576e3e7dccf58ae0429f74b6f170f61a656d
c-alvares <cyrobrandaoalvares@gmail.com>
2022-08-25 11:32:04 -0300

    Aula 03 de Backend, estabelecendo o back entre os bancos de dados criados

---
+++ 2DES/PWBE/AULA03/clientes/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
+
+const app = express();
+
+const con = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    database: 'clientes'  
+});
+
+// get, post, put, delete
+// busca, publica, altera, deletar
+
+app.get('/clientes',(req,res) => {
+    // res.json("Aoba! Wha 2 want");
+    let string = "select * from vw_clientes";
+    con.query(string,(err,result) =>{
+        if(err == null){
+            res.json(result);
+        }
+    });
+});
+
+app.listen(5000, () => {
+    console.log("Respondendo na porta 5000");
+});
