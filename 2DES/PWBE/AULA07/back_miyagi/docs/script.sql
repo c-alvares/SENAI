8a0022f709523f966cfdd4bc11012f41c159c971
c-alvares <cyrobrandaoalvares@gmail.com>
2022-09-08 08:54:51 -0300

    Projeto Aula 07 Loja do Miyagi

---
+++ 2DES/PWBE/AULA07/back_miyagi/docs/script.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS livro_caixa;
+CREATE DATABASE livro_caixa CHARSET=utf8 COLLATE UTF8_GENERAL_CI;
+USE livro_caixa;
+
+CREATE TABLE lancamentos (
+    n_lancamento INTEGER NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
+    data DATE NOT NULL,
+    descricao VARCHAR(30) NOT NULL,
+    valor FLOAT(5,2) NOT NULL,
+    tipo VARCHAR(1)
+);
+
+DESCRIBE lancamentos;
+
+SHOW TABLES;
+
+LOAD DATA INFILE 'C:/Users/Desenvolvimento/Desktop/SENAI/2DES/PWBE/AULA07/back_miyagi/docs/lancamentos.csv'
+-- LOAD DATA INFILE ''
+INTO TABLE lancamentos
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+SELECT * FROM lancamentos;
