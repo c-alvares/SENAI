467741805d33b65331e3ae85c187f1afd3f0b4f4
Cyro Alvares <cyrobrandaoalvares@gmail.com>
2022-09-20 19:03:00 -0300

    Atualizado cálculo de montante da tabela empréstimos

--- 2DES/BCD/AULA07/db/script.sql
+++ 2DES/BCD/AULA07/db/script.sql
@@ -54,38 +54,53 @@ DESCRIBE parcelas;
 
 SHOW TABLES;
 
-LOAD DATA INFILE 'C:/Users/Desenvolvimento/Desktop/SENAI/2DES/BCD/AULA07/db/docs/clientes.csv'
--- LOAD DATA INFILE ''
+-- LOAD DATA INFILE 'C:/Users/Desenvolvimento/Desktop/SENAI/2DES/BCD/AULA07/db/docs/clientes.csv'
+LOAD DATA INFILE 'C:/Users/oryc1/Desktop/SENAI/2DES/BCD/AULA07/db/docs/clientes.csv'
 INTO TABLE clientes
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
-LOAD DATA INFILE 'C:/Users/Desenvolvimento/Desktop/SENAI/2DES/BCD/AULA07/db/docs/telefones.csv'
--- LOAD DATA INFILE ''
+-- LOAD DATA INFILE 'C:/Users/Desenvolvimento/Desktop/SENAI/2DES/BCD/AULA07/db/docs/telefones.csv'
+LOAD DATA INFILE 'C:/Users/oryc1/Desktop/SENAI/2DES/BCD/AULA07/db/docs/telefones.csv'
 INTO TABLE telefones
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
-LOAD DATA INFILE 'C:/Users/Desenvolvimento/Desktop/SENAI/2DES/BCD/AULA07/db/docs/emprestimos.csv'
--- LOAD DATA INFILE ''
+-- LOAD DATA INFILE 'C:/Users/Desenvolvimento/Desktop/SENAI/2DES/BCD/AULA07/db/docs/emprestimos.csv'
+LOAD DATA INFILE 'C:/Users/oryc1/Desktop/SENAI/2DES/BCD/AULA07/db/docs/emprestimos.csv'
 INTO TABLE emprestimos
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
+UPDATE emprestimos SET montante = (SELECT SUM(capital*(pow((1+taxa_juros),n_parcelas))+impostos)) WHERE id = 1;
+UPDATE emprestimos SET montante = (SELECT SUM(capital*(pow((1+taxa_juros),n_parcelas))+impostos)) WHERE id = 2;
+UPDATE emprestimos SET montante = (SELECT SUM(capital*(pow((1+taxa_juros),n_parcelas))+impostos)) WHERE id = 3;
+UPDATE emprestimos SET montante = (SELECT SUM(capital*(pow((1+taxa_juros),n_parcelas))+impostos)) WHERE id = 4;
+UPDATE emprestimos SET montante = (SELECT SUM(capital*(pow((1+taxa_juros),n_parcelas))+impostos)) WHERE id = 5;
+UPDATE emprestimos SET montante = (SELECT SUM(capital*(pow((1+taxa_juros),n_parcelas))+impostos)) WHERE id = 6;
+UPDATE emprestimos SET montante = (SELECT SUM(capital*(pow((1+taxa_juros),n_parcelas))+impostos)) WHERE id = 7;
+UPDATE emprestimos SET montante = (SELECT SUM(capital*(pow((1+taxa_juros),n_parcelas))+impostos)) WHERE id = 8;
+UPDATE emprestimos SET montante = (SELECT SUM(capital*(pow((1+taxa_juros),n_parcelas))+impostos)) WHERE id = 9;
+UPDATE emprestimos SET montante = (SELECT SUM(capital*(pow((1+taxa_juros),n_parcelas))+impostos)) WHERE id = 10;
+
 SELECT * FROM clientes;
 SELECT * FROM telefones;
 SELECT * FROM emprestimos;
 
-INSERT INTO parcelas VALUES 
-(DEFAULT,)
 
-SELECT * FROM parcelas;
+
+-- INSERT INTO parcelas VALUES 
+-- (DEFAULT,DATE_ADD(CURDATE(),INTERVAL 10 MONTH,NULL,));
+
+-- SELECT * FROM parcelas;
 
 -- DROP TRIGGER IF EXISTS gera_parcela;
--- CREATE TRIGGER gera_parcela AFTER INSERT ON 
+-- CREATE TRIGGER calc_montante AFTER INSERT ON emprestimos
+--     FOR EACH ROW BEGIN
+--         INSERT INTO emprestimos VALUES
