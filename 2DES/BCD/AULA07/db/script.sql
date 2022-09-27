fde0d2f3dd0dcc226befe660040b95b9069ee901
Cyro Alvares <cyrobrandaoalvares@gmail.com>
2022-09-27 18:28:57 -0300

    Atualizações projeto Emprestimo

--- 2DES/BCD/AULA07/db/script.sql
+++ 2DES/BCD/AULA07/db/script.sql
@@ -54,24 +54,24 @@ DESCRIBE parcelas;
 
 SHOW TABLES;
 
-LOAD DATA INFILE 'C:/Users/Desenvolvimento/Desktop/SENAI/2DES/BCD/AULA07/db/docs/clientes.csv'
--- LOAD DATA INFILE 'C:/Users/oryc1/Desktop/SENAI/2DES/BCD/AULA07/db/docs/clientes.csv'
+-- LOAD DATA INFILE 'C:/Users/Desenvolvimento/Desktop/SENAI/2DES/BCD/AULA07/db/docs/clientes.csv'
+LOAD DATA INFILE 'C:/Users/oryc1/Desktop/SENAI/2DES/BCD/AULA07/db/docs/clientes.csv'
 INTO TABLE clientes
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
-LOAD DATA INFILE 'C:/Users/Desenvolvimento/Desktop/SENAI/2DES/BCD/AULA07/db/docs/telefones.csv'
--- LOAD DATA INFILE 'C:/Users/oryc1/Desktop/SENAI/2DES/BCD/AULA07/db/docs/telefones.csv'
+-- LOAD DATA INFILE 'C:/Users/Desenvolvimento/Desktop/SENAI/2DES/BCD/AULA07/db/docs/telefones.csv'
+LOAD DATA INFILE 'C:/Users/oryc1/Desktop/SENAI/2DES/BCD/AULA07/db/docs/telefones.csv'
 INTO TABLE telefones
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
-LOAD DATA INFILE 'C:/Users/Desenvolvimento/Desktop/SENAI/2DES/BCD/AULA07/db/docs/emprestimos.csv'
--- LOAD DATA INFILE 'C:/Users/oryc1/Desktop/SENAI/2DES/BCD/AULA07/db/docs/emprestimos.csv'
+-- LOAD DATA INFILE 'C:/Users/Desenvolvimento/Desktop/SENAI/2DES/BCD/AULA07/db/docs/emprestimos.csv'
+LOAD DATA INFILE 'C:/Users/oryc1/Desktop/SENAI/2DES/BCD/AULA07/db/docs/emprestimos.csv'
 INTO TABLE emprestimos
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
@@ -88,21 +88,50 @@ SELECT * FROM parcelas;
 -- DROP TRIGGER IF EXISTS geraParcela;
 -- CREATE TRIGGER geraParcela AFTER INSERT ON emprestimos
 --     FOR EACH ROW BEGIN
---         INSERT INTO emprestimos VALUES
---             (DEFAULT,DATE_ADD(SELECT data FROM emprestimos ),NULL,)
+--         INSERT INTO parcelas VALUES
+--             (id,DATE_ADD(SELECT data FROM emprestimos + SELECT n_parcelas FROM empretimos  ),NULL,)
 
 -- INSERT INTO parcelas VALUES 
 -- (DEFAULT,DATE_ADD(CURDATE(),INTERVAL 10 MONTH,NULL,));
 
 -- SELECT * FROM parcelas;
 
+-- Cálculo do Montante (OK)
+DROP TRIGGER IF EXISTS geraMontante;
+DELIMITER //
+CREATE TRIGGER
+    geraMontante BEFORE INSERT ON parcelas FOR EACH ROW
+    BEGIN
+        UPDATE emprestimos
+        SET montante = (SELECT SUM( capital * (pow(( 1 + taxa_juros / 100 ), n_parcelas)) + impostos )) where id = new.id;
+    END//
+DELIMITER ;
 
+-- Gerador de Parcelas (OK)
 DROP TRIGGER IF EXISTS geraParcela;
-CREATE TRIGGER 
-    geraParcela AFTER INSERT ON emprestimos FOR EACH ROW 
-    INSERT INTO parcelas VALUES (DEFAULT,curdate(),NULL,10, 200, val_recebido - valor);
+DELIMITER //
+CREATE TRIGGER  
+    geraParcela AFTER INSERT ON emprestimos FOR EACH ROW
+    BEGIN 
+        INSERT INTO parcelas VALUES (32,curdate(),NULL,10, 200, (val_recebido - valor));
+    END//
+DELIMITER ;
 
+
+-- Teste dos TRIGGERs
 INSERT INTO clientes VALUES ("123.456.789-12","Otto","Vonbell","13912-515",NULL);
 INSERT INTO telefones VALUES ("123.456.789-12","celular","16-982763-0237");
 INSERT INTO emprestimos VALUES (DEFAULT,"123.456.789-12",DATE_SUB(curdate(),INTERVAL 16 MONTH),2000.00,15,0.9,350,0);
-UPDATE emprestimos SET montante = (SELECT SUM( capital * (pow(( 1 + taxa_juros / 100 ), n_parcelas)) + impostos )) WHERE id != 0;
+-- UPDATE emprestimos SET montante = (SELECT SUM( capital * (pow(( 1 + taxa_juros / 100 ), n_parcelas)) + impostos )) WHERE id != 0;
+
+select * from clientes;
+select * from telefones;
+select * from emprestimos order by id desc limit 1;
+
+-- PERGUNTAS! 
+-- Como atribuir o ID (chave primária) de empréstimos no gerador de parcelas para tabela parcelas?
+-- Na linha 92, como fazer cálculo entre um tipo datetime e uma integer?
+-- Na linha 92, há alguma forma de selecionar os dados da tabela emprestimos para inserí-los em parcelas sem ficar enorme?
+
+
+
