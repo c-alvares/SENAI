ea9b018e9003106c67fa310b3d87b22ac305d128
Cyro Alvares <cyrobrandaoalvares@gmail.com>
2022-11-10 14:34:29 -0300

    .

--- 2DES/projeto1/mobile/mobile/App.js
+++ 2DES/projeto1/mobile/mobile/App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View, TextInput, Image, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
 
 import styles from './style';
 
 export default function App() {
 
-  const [  nome, setNome ] = useState("")
-  const [  ,  ] = useState("")
-  const [  ,  ] = useState("")
-  const [  ,  ] = useState("")
-  const [  ,  ] = useState("")
-  const [  ,  ] = useState("")
-  const [  ,  ] = useState("")
-  const [  ,  ] = useState("")
-  const [  ,  ] = useState("")
-  const [  ,  ] = useState("")
+  const [ nome, setNome ] = useState("")
+  const [ cpf , setCpf ] = useState("")
+  const [ telefone , setTelefone ] = useState("")
+  const [ tipo , setTipo ] = useState("")
+  const [ logradouro , setLogradouro ] = useState("")
+  const [ rua , setRua ] = useState("")
+  const [ bairro , setBairro ] = useState("")
+  const [ numero , setNumero ] = useState("")
+  const [ cidade , setCidade ] = useState("")
+  const [ estado , setEstado ] = useState("")
 
 
   return (
@@ -23,16 +24,16 @@ export default function App() {
         <Image style={styles.logo} source={require('./assets/logo.png')} />
         <Text style={styles.titulo}>Cadastro de Pessoas</Text>
         <View style={styles.container2}>
-          <TextInput onChangeText=(text => this.setNome({ nome: text })) style={styles.inputs} placeholder="Nome" placeholderTextColor="lightgray"></TextInput>
-          <TextInput onChangeText=(text => this.setCpf({ cpf: text })) style={styles.inputs} placeholder="CPF" placeholderTextColor="lightgray"></TextInput>
-          <TextInput onChangeText=(text => this.setTelefone({ telefone: text })) style={styles.inputs} placeholder="DDD + Telefone" placeholderTextColor="lightgray"></TextInput>
-          <TextInput onChangeText=(text => this.setTipo({ tipo: text })) style={styles.inputs} placeholder="Tipo" placeholderTextColor="lightgray"></TextInput>
-          <TextInput onChangeText=(text => this.setLogradouro({ logradouro: text })) style={styles.inputs} placeholder="Logradouro" placeholderTextColor="lightgray"></TextInput>
-          <TextInput onChangeText=(text => this.setRua({ rua: text })) style={styles.inputs} placeholder="Rua" placeholderTextColor="lightgray"></TextInput>
-          <TextInput onChangeText=(text => this.setBairro({ bairro: text })) style={styles.inputs} placeholder="Bairro" placeholderTextColor="lightgray"></TextInput>
-          <TextInput onChangeText=(text => this.setNumero({ numero: text })) style={styles.inputs} placeholder="Número da Casa" placeholderTextColor="lightgray"></TextInput>
-          <TextInput onChangeText=(text => this.setCidade({ cidade: text })) style={styles.inputs} placeholder="Cidade" placeholderTextColor="lightgray"></TextInput>
-          <TextInput onChangeText=(text => this.setEstado({ estado: text })) style={styles.inputs} placeholder="Estado" placeholderTextColor="lightgray"></TextInput>
+          <TextInput onChangeText={(text) => setNome({ nome: text })} style={styles.inputs} placeholder="Nome" placeholderTextColor="lightgray"></TextInput>
+          <TextInput onChangeText={(text) => setCpf({ cpf: text })} style={styles.inputs} placeholder="CPF" placeholderTextColor="lightgray"></TextInput>
+          <TextInput onChangeText={(text) => setTelefone({ telefone: text })} style={styles.inputs} placeholder="DDD + Telefone" placeholderTextColor="lightgray"></TextInput>
+          <TextInput onChangeText={(text) => setTipo({ tipo: text })} style={styles.inputs} placeholder="Tipo" placeholderTextColor="lightgray"></TextInput>
+          <TextInput onChangeText={(text) => setLogradouro({ logradouro: text })} style={styles.inputs} placeholder="Logradouro" placeholderTextColor="lightgray"></TextInput>
+          <TextInput onChangeText={(text) => setRua({ rua: text })} style={styles.inputs} placeholder="Rua" placeholderTextColor="lightgray"></TextInput>
+          <TextInput onChangeText={(text) => setBairro({ bairro: text })} style={styles.inputs} placeholder="Bairro" placeholderTextColor="lightgray"></TextInput>
+          <TextInput onChangeText={(text) => setNumero({ numero: text })} style={styles.inputs} placeholder="Número da Casa" placeholderTextColor="lightgray"></TextInput>
+          <TextInput onChangeText={(text) => setCidade({ cidade: text })} style={styles.inputs} placeholder="Cidade" placeholderTextColor="lightgray"></TextInput>
+          <TextInput onChangeText={(text) => setEstado({ estado: text })} style={styles.inputs} placeholder="Estado" placeholderTextColor="lightgray"></TextInput>
           <TouchableOpacity style={styles.button}>
             <Text style={styles.buttonText}>CADASTRAR</Text>
           </TouchableOpacity>
