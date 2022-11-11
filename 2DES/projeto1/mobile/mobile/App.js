c0f2047e995a2aafffd69553ae74cd5e279c04a2
Cyro Alvares <cyrobrandaoalvares@gmail.com>
2022-11-11 05:58:23 -0300

    Falta o Fetch

--- 2DES/projeto1/mobile/mobile/App.js
+++ 2DES/projeto1/mobile/mobile/App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View, TextInput, Image, TouchableOpacity } from 'react-native';
 import React, { useState } from 'react';
-
+// https://www.alura.com.br/artigos/react-native-fazendo-requisicoes-fetch?gclid=Cj0KCQiAgribBhDkARIsAASA5bsduZAJs5u31SweE7QSf0VLrgAFRUAh_sR6XRQ0KLWA6MyKZQqo5WoaAoyYEALw_wcB
+// https://reactnative.dev/docs/network
 import styles from './style';
 
 export default function App() {
@@ -16,7 +17,36 @@ export default function App() {
   const [ numero , setNumero ] = useState("")
   const [ cidade , setCidade ] = useState("")
   const [ estado , setEstado ] = useState("")
+  const [ lista, setLista ] = ([
+    {
+      "nome":"Otto",
+      "cpf":"123456789-12",
+      "telefone":"19-98541-2213",
+      "tipo":"celular",
+      "logradouro":"Avenida",
+      "rua":"das Bandeiras",
+      "bairro":"Elísios",
+      "numero":"774",
+      "cidade":"Mairiporã",
+      "estado":"SP"
+    }
+  ]);
 
+  const cadastrar = () => {
+    let nFunc = {
+      "nome":nome,
+      "cpf":cpf,
+      "telefone":telefone,
+      "tipo":tipo,
+      "logradouro":logradouro,
+      "rua":rua,
+      "bairro":bairro,
+      "numero":numero,
+      "cidade":cidade,
+      "estado":estado
+    }
+    setLista([...lista, nFunc]);
+  }
 
   return (
     <View style={styles.container}>
@@ -24,17 +54,17 @@ export default function App() {
         <Image style={styles.logo} source={require('./assets/logo.png')} />
         <Text style={styles.titulo}>Cadastro de Pessoas</Text>
         <View style={styles.container2}>
-          <TextInput onChangeText={(text) => setNome({ nome: text })} style={styles.inputs} placeholder="Nome" placeholderTextColor="lightgray"></TextInput>
-          <TextInput onChangeText={(text) => setCpf({ cpf: text })} style={styles.inputs} placeholder="CPF" placeholderTextColor="lightgray"></TextInput>
-          <TextInput onChangeText={(text) => setTelefone({ telefone: text })} style={styles.inputs} placeholder="DDD + Telefone" placeholderTextColor="lightgray"></TextInput>
-          <TextInput onChangeText={(text) => setTipo({ tipo: text })} style={styles.inputs} placeholder="Tipo" placeholderTextColor="lightgray"></TextInput>
-          <TextInput onChangeText={(text) => setLogradouro({ logradouro: text })} style={styles.inputs} placeholder="Logradouro" placeholderTextColor="lightgray"></TextInput>
-          <TextInput onChangeText={(text) => setRua({ rua: text })} style={styles.inputs} placeholder="Rua" placeholderTextColor="lightgray"></TextInput>
-          <TextInput onChangeText={(text) => setBairro({ bairro: text })} style={styles.inputs} placeholder="Bairro" placeholderTextColor="lightgray"></TextInput>
-          <TextInput onChangeText={(text) => setNumero({ numero: text })} style={styles.inputs} placeholder="Número da Casa" placeholderTextColor="lightgray"></TextInput>
-          <TextInput onChangeText={(text) => setCidade({ cidade: text })} style={styles.inputs} placeholder="Cidade" placeholderTextColor="lightgray"></TextInput>
-          <TextInput onChangeText={(text) => setEstado({ estado: text })} style={styles.inputs} placeholder="Estado" placeholderTextColor="lightgray"></TextInput>
-          <TouchableOpacity style={styles.button}>
+          <TextInput onChangeText={(text) => setNome(text)} style={styles.inputs} placeholder="Nome" placeholderTextColor="lightgray"></TextInput>
+          <TextInput onChangeText={(text) => setCpf(text)} style={styles.inputs} placeholder="CPF" placeholderTextColor="lightgray"></TextInput>
+          <TextInput onChangeText={(text) => setTelefone(text)} style={styles.inputs} placeholder="DDD + Telefone" placeholderTextColor="lightgray"></TextInput>
+          <TextInput onChangeText={(text) => setTipo(text)} style={styles.inputs} placeholder="Tipo" placeholderTextColor="lightgray"></TextInput>
+          <TextInput onChangeText={(text) => setLogradouro(text)} style={styles.inputs} placeholder="Logradouro" placeholderTextColor="lightgray"></TextInput>
+          <TextInput onChangeText={(text) => setRua(text)} style={styles.inputs} placeholder="Rua" placeholderTextColor="lightgray"></TextInput>
+          <TextInput onChangeText={(text) => setBairro(text)} style={styles.inputs} placeholder="Bairro" placeholderTextColor="lightgray"></TextInput>
+          <TextInput onChangeText={(text) => setNumero(text)} style={styles.inputs} placeholder="Número da Casa" placeholderTextColor="lightgray"></TextInput>
+          <TextInput onChangeText={(text) => setCidade(text)} style={styles.inputs} placeholder="Cidade" placeholderTextColor="lightgray"></TextInput>
+          <TextInput onChangeText={(text) => setEstado(text)} style={styles.inputs} placeholder="Estado" placeholderTextColor="lightgray"></TextInput>
+          <TouchableOpacity onPress={() => { cadastrar() }} style={styles.button}>
             <Text style={styles.buttonText}>CADASTRAR</Text>
           </TouchableOpacity>
           <StatusBar style="auto" />
