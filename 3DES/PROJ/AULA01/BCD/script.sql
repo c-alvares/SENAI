1cac293c87eb017d484686ce07458d44c0815774
c-alvares <cyrobrandaoalvares@gmail.com>
2023-01-23 10:33:16 -0300

    Finalizada estrutura do bd e inseridos os dados csv

--- 3DES/PROJ/AULA01/BCD/script.sql
+++ 3DES/PROJ/AULA01/BCD/script.sql
@@ -1,2 +1,52 @@
 DROP DATABASE IF EXISTS IRango;
-CREATE DATABASE IRango CHARSET utf8 COLLATE utf8_;
+CREATE DATABASE IRango CHARSET=utf8 COLLATE utf8_general_ci;
+USE IRango;
+
+CREATE TABLE entregadores(
+    ID_Entregador NUMERIC(4) NOT NULL,
+    Nome VARCHAR(50) NOT NULL,
+    Email VARCHAR(50) NOT NULL,
+    Senha VARCHAR(50) NOT NULL,
+    Veiculo VARCHAR(50) NOT NULL,
+    CONSTRAINT pk_entregador primary key (ID_Entregador)
+);
+
+CREATE TABLE pedidos(
+    ID_Pedido NUMERIC(4) NOT NULL,
+    Cliente VARCHAR(50) NOT NULL,
+    Endereco VARCHAR(50) NOT NULL,
+    Produto VARCHAR(50) NOT NULL,
+    data DATE NOT NULL,
+    Hora_pedido TIME NOT NULL,
+    Hora_entrega TIME NOT NULL,
+    Hora_fim TIME NULL,
+    Entregador NUMERIC(4) NOT NULL,
+    CONSTRAINT pk_pedido primary key (ID_Pedido),
+    FOREIGN KEY (Entregador) REFERENCES entregadores(ID_Entregador)
+);
+
+DESCRIBE entregadores;
+DESCRIBE pedidos;
+
+-- Importação dos dados com LOAD DATA de arquivos CSV
+
+LOAD DATA INFILE 'C:/Users/Desenvolvimento/Desktop/SENAI/3DES/PROJ/AULA01/DADOS/entregadores.csv'
+-- LOAD DATA INFILE 'C:/Users/Desenvolvimento/Desktop/SENAI/3DES/PROJ/AULA01/DADOS/entregadores.csv'
+INTO TABLE entregadores
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+SELECT * FROM entregadores;
+
+
+LOAD DATA INFILE 'C:/Users/Desenvolvimento/Desktop/SENAI/3DES/PROJ/AULA01/DADOS/pedidos.csv'
+-- LOAD DATA INFILE 'C:/Users/Desenvolvimento/Desktop/SENAI/3DES/PROJ/AULA01/DADOS/entregadores.csv'
+INTO TABLE pedidos
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+SELECT * FROM pedidos;
