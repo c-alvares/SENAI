22a240d984082ef9eb2a97251b6e03d8ec873c7c
c-alvares <cyrobrandaoalvares@gmail.com>
2023-01-24 11:33:44 -0300

    API estabelecida e executada.

--- 3DES/PROJ/AULA01/BACKEND/index.js
+++ 3DES/PROJ/AULA01/BACKEND/index.js
@@ -4,11 +4,13 @@ const express = require('express');
 const cors = require('cors');
 
 const entregadores = require('./src/routes/Entregadores.routes');
+const pedidos = require('./src/routes/Pedidos.routes');
 
 const app = express()
     .use(express.json())
     .use(cors())
     .use(entregadores)
+    .use(pedidos)
 
 app.listen(PORT, () => {
     console.log('Servidor em execução na porta ' + PORT);
