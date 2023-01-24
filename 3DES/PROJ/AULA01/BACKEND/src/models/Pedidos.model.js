740dca01300dec6c643127e7b15fef8e5355775e
c-alvares <cyrobrandaoalvares@gmail.com>
2023-01-24 11:03:27 -0300

    Implementadas requisições de pedidos para BD

---
+++ 3DES/PROJ/AULA01/BACKEND/src/models/Pedidos.model.js
@@ -0,0 +1,45 @@
+// Cria novo pedido
+const novoPedido = (model) => {
+    return `INSERT INTO pedidos VALUES (DEFAULT,'${model.Cliente}','${model.Endereco}','${model.Produto}',CURDATE(),CURTIME(),NULL,NULL,'${model.Entregador}')`;
+}
+
+
+// Lista todos os pedidos
+const mostrarPedidos = () => {
+    return `SELECT * FROM pedidos`;
+}
+
+// Lista um pedido pelo nome do cliente
+const mostrarPedidoPorCliente = (model) => {
+    return `SELECT * FROM pedidos WHERE Cliente LIKE '%${model.Cliente}%'`;
+}
+
+
+// Atualiza hora de término do pedido pelo nome do cliente
+const atualizarPedidoPronto = (model) => {
+    return `UPDATE pedidos SET 
+                Hora_entrega = '${model.Hora_entrega}' 
+            WHERE Cliente LIKE '%${model.Cliente}%' AND Hora_entrega = 'NULL'`;
+}
+
+// Atualiza hora de entrega do pedido pelo nome do cliente
+const atualizarPedidoEntregue = (model) => {
+    return `UPDATE pedidos SET 
+                Hora_fim = '${model.Hora_fim}' 
+            WHERE Cliente LIKE '%${model.Cliente}%' AND Hora_fim = 'NULL'`;
+}
+
+
+// Exclui um pedido
+const excluirPedido = (model) => {
+    return `DELETE FROM pedidos WHERE Cliente LIKE '%${model.Cliente}%' AND Hora_entrega = 'NULL'`;
+}
+
+module.exports = {
+    novoPedido,
+    mostrarPedidos,
+    mostrarPedidoPorCliente,
+    atualizarPedidoPronto,
+    atualizarPedidoEntregue,
+    excluirPedido
+}
