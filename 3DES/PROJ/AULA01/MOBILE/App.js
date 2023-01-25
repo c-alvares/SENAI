5776056f636e69ddd84f928290b5f4a50defe142
c-alvares <cyrobrandaoalvares@gmail.com>
2023-01-25 11:32:23 -0300

    Função para finalizar pedido incompleta

--- 3DES/PROJ/AULA01/MOBILE/App.js
+++ 3DES/PROJ/AULA01/MOBILE/App.js
@@ -4,10 +4,10 @@ import { View, Text, Image, TextInput } from "react-native";
 
 import styles from "./styles/style";
 
-import ButtonPedidoPronto from './components/buttonPedidoPronto';
+import ButtonPedidoPronto from "./components/buttonPedidoPronto";
 
 export default function App() {
-
+  // Consumo da API para importação dos pedidos a serem preparados
   const [order, setOrder] = useState([]);
 
   fetch("http://localhost:3000/pedidosapreparar")
@@ -17,6 +17,23 @@ export default function App() {
     .then((dados) => {
       setOrder(dados);
     });
+  // Fim do consumo da API
+
+  // Função para finalizar pedido
+  const [concluir, setConcluir] = useState([]);
+  // jogar o valor do pedido.Cliente para o setConcluir e então fazer o fetch
+
+  const finalizar = (concluir) => {
+    console.log();
+    
+    // fetch("http://localhost:3000/atualizarpedidopronto")
+    //   .then((resp) => {
+    //  ;   return resp.json();
+    //   })
+    //   .then((dados) => {
+    //     setConcluir(dados);
+    //   })
+  };
 
   return (
     <View style={styles.container}>
@@ -32,15 +49,17 @@ export default function App() {
             <Text style={styles.dados}>Endereço: {pedido.Endereco}</Text>
             <Text style={styles.dados}>Produto: {pedido.Produto}</Text>
             <Text style={styles.dados}>Data: {pedido.data}</Text>
-            <Text style={styles.dados}>Hora do Pedido: {pedido.Hora_pedido}</Text>
+            <Text style={styles.dados}>
+              Hora do Pedido: {pedido.Hora_pedido}
+            </Text>
             {/* <Text style={styles.dados}>{pedido.Hora_entrega}</Text>
             <Text style={styles.dados}>{pedido.Hora_fim}</Text> */}
             <Text style={styles.dados}>Entregador: {pedido.nome}</Text>
-            <ButtonPedidoPronto value="PEDIDO PRONTO PARA ENTREGA"></ButtonPedidoPronto>
+            
+            <ButtonPedidoPronto value="PEDIDO PRONTO PARA ENTREGA"onPress={finalizar(setConcluir)}></ButtonPedidoPronto>
           </View>
         );
       })}
-
     </View>
   );
 }
