171f736db83ef59b48942fe51db8fe80f67a636d
c-alvares <cyrobrandaoalvares@gmail.com>
2023-01-30 08:38:53 -0300

    Finalizada importação das rotas e dependências

--- 3DES/PROJ/AULA02/index.js
+++ 3DES/PROJ/AULA02/index.js
@@ -3,7 +3,10 @@ const cors = require('cors');
 
 const app = express();
 
+const router = require('./src/routes/routes')
+
 app.use(cors());
+app.use(router);
 
 app.listen(3000, () => {
     console.log("Listening on port 3000")
