d58e28840512186a817c7d329ac1909b2a39ff48
c-alvares <cyrobrandaoalvares@gmail.com>
2023-02-03 11:32:24 -0300

    Adicionadas rotas de vendas e detalhes

--- 3DES/PROJ/AULA03/BACKEND/index.js
+++ 3DES/PROJ/AULA03/BACKEND/index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const setor = require('./src/routes/setor.route');
 const produtos = require('./src/routes/produtos.route');
 const vendedores = require('./src/routes/vendedores.route')
+const vendas = require('./src/routes/vendas.route');
+const detalhes = require('./src/routes/detalhes.route');
 
 const app = express();
 
@@ -12,5 +14,7 @@ app.use(express.json());
 app.use(setor);
 app.use(produtos);
 app.use(vendedores);
+app.use(vendas);
+app.use(detalhes);
 
 app.listen(3000, () => { console.log("Rodando"); })
