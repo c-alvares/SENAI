254d1241e17a484d669d9eee600a1b555e4324fa
c-alvares <cyrobrandaoalvares@gmail.com>
2023-02-03 09:13:53 -0300

    Criadas duas novas tabelas

--- 3DES/PROJ/AULA03/BACKEND/prisma/migrations/20230203121212_init/migration.sql
+++ 3DES/PROJ/AULA03/BACKEND/prisma/migrations/20230203121212_init/migration.sql
@@ -27,8 +27,36 @@ CREATE TABLE `Vendedor` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `Vendas` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `data` DATETIME(3) NOT NULL,
+    `id_vendedor` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Detalhes` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id_produto` INTEGER NOT NULL,
+    `id_venda` INTEGER NOT NULL,
+    `quantidade` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `Produtos` ADD CONSTRAINT `Produtos_setor_fkey` FOREIGN KEY (`setor`) REFERENCES `Setor`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `Vendedor` ADD CONSTRAINT `Vendedor_setor_fkey` FOREIGN KEY (`setor`) REFERENCES `Setor`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Vendas` ADD CONSTRAINT `Vendas_id_vendedor_fkey` FOREIGN KEY (`id_vendedor`) REFERENCES `Vendedor`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Detalhes` ADD CONSTRAINT `Detalhes_id_produto_fkey` FOREIGN KEY (`id_produto`) REFERENCES `Produtos`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Detalhes` ADD CONSTRAINT `Detalhes_id_venda_fkey` FOREIGN KEY (`id_venda`) REFERENCES `Vendas`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
