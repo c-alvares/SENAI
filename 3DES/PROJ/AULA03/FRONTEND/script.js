2cd1b3fa9fc11da8feae8b4aba5fd6aa1ebe1eee
c-alvares <cyrobrandaoalvares@gmail.com>
2023-02-14 06:20:42 -0300

    Bug para listar  setores

--- 3DES/PROJ/AULA03/FRONTEND/script.js
+++ 3DES/PROJ/AULA03/FRONTEND/script.js
@@ -2,7 +2,10 @@ let input_name = document.querySelector('#input_name');
 let input_comission = document.querySelector('#input_comission');
 let btn_register = document.querySelector('#btn_register');
 
+let sector_structure = document.querySelector('#sector_structure');
+
 const uri_create_sector = 'http://localhost:3000/criarsetor/';
+const uri_list_sectores = 'http://localhost:3000/buscarsetores';
 
 function createSector() {
     let create = {
@@ -27,4 +30,19 @@ function createSector() {
             }
         })
         .catch(err => console.error(err));
+}
+
+function loadSectors() {
+    fetch(uri_list_sectores)
+        .then(response => response.json())
+        .then(response => {
+            response.forEach(sector => {
+                let sector_line = document.querySelector('#sector_line1').cloneNode(true)
+
+                sector_line.querySelector('#id_cell').innerHTML = sector.id
+                // sector_line.querySelector('#name_cell').innerHTML = sector.nome
+                // sector_line.querySelector('#comission_cell').innerHTML = sector.comissao
+                sector_structure.appendChild(sector_line)
+            })
+        })
 }
